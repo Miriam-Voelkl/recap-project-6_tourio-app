20aca9bcce42999efdec2928b790506c9115bb9b
Miriam Völkl <miriamvoelkl@gmail.com>
2024-01-26 16:21:57 +0100

    add edit function and api route

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -3,18 +3,31 @@ import Place from "@/db/models/Place";
 
 export default async function handler(request, response) {
   await dbConnect();
-  const { id } = request.query;
 
-  if (!id) {
-    return;
-  }
+  try {
+    const { id } = request.query;
 
-  if (request.method === "GET") {
-    const place = await Place.findById(id);
+    if (request.method === "GET") {
+      const place = await Place.findById(id);
 
-    if (!place) {
-      return response.status(404).json({ status: "Not found" });
+      if (!place) {
+        return response.status(404).json({ status: "Not found" });
+      }
+      response.status(200).json(place);
+    } else if (request.method === "PUT") {
+      const placeData = request.body;
+      await Place.findByIdAndUpdate(id, placeData);
+      return response.status(200).json({ status: `Place ${id} updated!` });
+    } else {
+      response
+        .status(405)
+        .json({ status: "error", message: "Method not allowed" });
     }
-    response.status(200).json(place);
+  } catch (error) {
+    response.status(500).json({
+      status: error,
+      message: "Internal Server Error",
+      error: error.message,
+    });
   }
 }
